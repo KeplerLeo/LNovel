aadc624caf722afe6b87718ea0c5d4cadcefb704
Leonardo Kepler Mesquita <leokepler@gmail.com>
2023-11-07 17:19:47 -0300

    tentando

--- index.js
+++ index.js
@@ -2,12 +2,26 @@ const puppeteer = require('puppeteer');
 const prompt = require('prompt-sync')();
 
 (async () => {
-  const browser = await puppeteer.launch({headless: false});
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto('https://centralnovel.com/series/the-beginning-after-the-end/');
 
   const vol = prompt('Volume: ');
   console.log(`Volume ${vol}`);
+
+  await page.evaluate((vol) => {
+    const spans = document.querySelectorAll('span.ts-chl-collapsible');
+    console.log(spans);
+    for (const span of spans) {
+      console.log(span.textContent);
+      if (span.textContent === `Volume ${vol}`) {
+        span.click();
+        break;
+      }
+    }
+  }, vol);
+
+  const end = prompt('End? ');
 
   await browser.close();
 }
