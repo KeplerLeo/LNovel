04af42a5c7680793c4a36ac7028c6bd90e597a2e
Leonardo Kepler Mesquita <leokepler@gmail.com>
2023-11-07 17:04:21 -0300

    seleção de volume

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-//import puppeteer from 'puppeteer';
 const puppeteer = require('puppeteer');
+const prompt = require('prompt-sync')();
 
 (async () => {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
   await page.goto('https://centralnovel.com/series/the-beginning-after-the-end/');
-  await page.waitFor(1000);
 
-  //Get the title of the page
-  const title = await page.title();
-  console.log('Title: ' + title);
-
-  //Get the url of the page
-  const url = await page.url();
-  console.log('URL: ' + url);
-
-  //Get the text content of the page
-  const text = await page.content();
-  console.log('Text: ' + text);
+  const vol = prompt('Volume: ');
+  console.log(`Volume ${vol}`);
 
   await browser.close();
 }
